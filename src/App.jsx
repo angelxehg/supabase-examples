50b88d0a136cace48404f74e79908f9f07c91874
Angel Hurtado <contact@angelxehg.com>
2021-04-05 13:33:29 -0500

    Importar Button directamente (-74 b)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import { createClient } from '@supabase/supabase-js'
 
 import './App.css';
