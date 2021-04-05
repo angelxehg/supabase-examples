6c9f29bf5faa7c418324ce3dbfed5ee768606a7e
Angel Hurtado <contact@angelxehg.com>
2021-04-05 12:22:16 -0500

    Instalar Bootstrap 5 beta 3

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
