b49008dc352d9b076ae47c1ceace5b296ae94184
Angel Hurtado <contact@angelxehg.com>
2021-04-05 12:17:27 -0500

    Eliminar plantilla inicial

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hola mundo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hola mundo/i);
   expect(linkElement).toBeInTheDocument();
 });
